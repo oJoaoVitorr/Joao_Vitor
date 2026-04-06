110483f81962e22ab77154906eea9e83a7c8ca20
oJoaoVitorr <aserola30@gmail.com>
2026-04-06 12:57:34 -0300

    Exercicio 21 finalizado.

---
+++ Joao_Vitor/Exercicio21.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Joao_Vitor
+{
+    public partial class Exercicio21 : Form
+    {
+        public Exercicio21()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nome, sobrenome, nomef;
+            nome = textBox1.Text;
+            sobrenome = textBox2.Text;
+            nomef = nome + " " + sobrenome;
+
+            label4.Text= nomef;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            label4.Text = "";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
