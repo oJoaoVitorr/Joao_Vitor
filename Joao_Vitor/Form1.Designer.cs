e2d6f683a9a9f3413aeef334851abd48827a8816
oJoaoVitorr <aserola30@gmail.com>
2026-04-15 13:40:52 -0300

    Agora o menu inicia no centro da tela (tinha esquecido).

--- Joao_Vitor/Form1.Designer.cs
+++ Joao_Vitor/Form1.Designer.cs
@@ -361,8 +361,10 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Lista de Exercicios 1";
             this.ResumeLayout(false);
             this.PerformLayout();
